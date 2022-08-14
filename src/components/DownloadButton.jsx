1bbb37899df3cbe157d23ffa9c7f2b0ead79bf4b
Alejo Janin <alejojanin@gmail.com>
2022-08-14 11:08:03 -0300

    Fix fetch of image in DownloadButton

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,12 +1,13 @@
 import { saveAs } from "file-saver";
 
 export default function DownloadButton(props) {
-  function downloadImage() {
-    fetch(props.randomImage)
+  async function downloadImage() {
+    await fetch(props.randomImage)
       .then((res) => res.blob()) // Gets the response and returns it as a blob
       .then((blob) => {
         saveAs(blob, "image");
       });
   }
+
   return <button onClick={downloadImage}>Download</button>;
 }
