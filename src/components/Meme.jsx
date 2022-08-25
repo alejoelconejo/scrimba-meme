bcd2e6a092020b6fa6c368d2a6505809fc8fe575
Alejo Janin <alejojanin@gmail.com>
2022-08-25 09:01:27 -0300

    Fix text-shadow in meme text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -114,13 +114,13 @@ export default function Meme() {
             alt={meme.name}
           />
           <h2
-            className="font-meme absolute -translate-x-1/2 w-2/3 text-center select-none left-1/2 my-4 mx-0 py-0 px-1 drop-shadow-meme uppercase text-white top-0"
+            className="font-meme absolute -translate-x-1/2 w-2/3 text-center select-none left-1/2 my-4 mx-0 py-0 px-1 [text-shadow:2px_2px_0_#000,-2px_-2px_0_#000,2px_-2px_0_#000,-2px_2px_0_#000,_0_2px_0_#000,_2px_0_0_#000,_0_-2px_0_#000,_-2px_0_0_#000,2px_2px_5px_#000] uppercase text-white top-0"
             style={{ fontSize: `${fontSize}em` }}
           >
             {meme.topText}
           </h2>
           <h2
-            className="font-meme absolute -translate-x-1/2 w-2/3 text-center select-none left-1/2 my-4 mx-0 py-0 px-1 drop-shadow-meme uppercase text-white bottom-0"
+            className="font-meme absolute -translate-x-1/2 w-2/3 text-center select-none left-1/2 my-4 mx-0 py-0 px-1 [text-shadow:2px_2px_0_#000,-2px_-2px_0_#000,2px_-2px_0_#000,-2px_2px_0_#000,_0_2px_0_#000,_2px_0_0_#000,_0_-2px_0_#000,_-2px_0_0_#000,2px_2px_5px_#000] uppercase text-white bottom-0"
             style={{ fontSize: `${fontSize}em` }}
           >
             {meme.bottomText}
