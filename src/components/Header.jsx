38f0ec245b8873fb363e8c6296d85b1201a11cbf
Alejo Janin <alejojanin@gmail.com>
2022-08-29 15:22:16 -0300

    Add new title to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,10 @@ export default function Header() {
     <div className="flex justify-between items-center py-4 px-6 max-w-3xl mx-auto rounded bg-gradient-to-r from-indigo-500 to-pink-500">
       <div className="flex justify-center items-center gap-2">
         <img className="w-8" src={trollFace} />
-        <h1 className="font-bold text-xl leading-6 tracking-tight">
-          Meme Generator
-        </h1>
+        <div className="flex gap-2 items-baseline">
+          <h1 className="font-bold text-3xl tracking-tight">Memardo</h1>
+          <p className=" sm:block hidden">another meme generator</p>
+        </div>
       </div>
     </div>
   )
