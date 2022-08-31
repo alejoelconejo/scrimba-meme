81f6ea49251207eb15510a2593414afac284f623
Alejo Janin <alejojanin@gmail.com>
2022-08-31 07:20:35 -0300

    Add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Meme from "./components/Meme"
 
 function App() {
   return (
-    <div className="font-sans text-white bg-gray-50 w-full h-full dark:bg-slate-900">
+    <div className="font-sans text-white bg-gray-50 w-full h-full dark:bg-slate-900 transition-colors duration-500">
       <Header />
       <Meme />
     </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ const Input = ({ placeholder, onChange, value, name }) => {
   return (
     <>
       <input
-        className="font-sans text-lg text-slate-800 flex-1 rounded p-2 border-gray-300 border-2 dark:border-none"
+        className="font-sans text-lg text-slate-800 flex-1 rounded p-2 border-gray-300 border-2 dark:border-transparent"
         placeholder={placeholder}
         onChange={onChange}
         value={value}
