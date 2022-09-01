788353c83d7726e9fd7d72bd4c4d59cfb2be15da
Alejo Janin <alejojanin@gmail.com>
2022-09-01 07:39:07 -0300

    Add svg icons

--- src/components/GetMemeButton.jsx
+++ src/components/GetMemeButton.jsx
@@ -1,14 +1,16 @@
 import getMemeImage from "../services/getMemeImage"
+import { ReactComponent as GetMemeImage } from "../images/get-meme-button.svg"
 
 const GetMemeButton = ({ allMemes, setMeme }) => {
   return (
     <button
-      className="rounded font-bold text-lg p-2 font-sans  bg-violet-500 w-fit mx-auto px-4"
+      className="rounded font-bold text-lg p-2 font-sans  bg-violet-500 w-fit mx-auto px-4 flex gap-2 items-end"
       onClick={() => {
         getMemeImage({ allMemes, setMeme })
       }}
     >
-      Get a new meme image 🖼️
+      Get a new meme image
+      <GetMemeImage className="h-6" />
     </button>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react"
 import trollFace from "../images/troll-face.png"
+import { ReactComponent as DarkIcon } from "../images/dark-icon.svg"
+import { ReactComponent as LightIcon } from "../images/light-icon.svg"
 
 const Header = () => {
   // Change the icons inside the button based on previous settings
@@ -58,9 +60,9 @@ const Header = () => {
 
   return (
     <header className="flex justify-between items-center py-4 px-6 max-w-3xl mx-auto rounded bg-gradient-to-r from-indigo-500 to-pink-500">
-      <div className="flex justify-center items-center gap-2">
+      <div className="flex flex-1 justify-center items-center gap-2">
         <img className="w-8" src={trollFace} />
-        <div className="flex gap-2 items-baseline">
+        <div className="flex gap-2 items-baseline flex-1">
           <h1 className="font-bold text-3xl tracking-tight">Memardo</h1>
           <p className=" sm:block hidden">another meme generator</p>
         </div>
@@ -68,30 +70,10 @@ const Header = () => {
           id="theme-toggle"
           type="button"
           onClick={handleClickDark}
-          className="text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+          className="text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5"
         >
-          <svg
-            id="theme-toggle-dark-icon"
-            className="hidden w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-          </svg>
-          <svg
-            id="theme-toggle-light-icon"
-            className="hidden w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+          <DarkIcon />
+          <LightIcon />
         </button>
       </div>
     </header>
