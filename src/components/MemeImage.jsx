0aadaed0d951b021783e81cbbc3b69c7688d51d1
Alejo Janin <alejojanin@gmail.com>
2022-09-17 08:41:29 -0300

    Add text wrap to div width in meme image

--- src/components/MemeImage.jsx
+++ src/components/MemeImage.jsx
@@ -8,7 +8,7 @@ const MemeImage = ({ meme, fontSize }) => {
         title={meme.name}
       />
       <h2
-        className='font-meme absolute -translate-x-1/2 w-2/3 text-center select-none left-1/2 my-4 mx-0 py-0 px-1 [text-shadow:2px_2px_0_#000,-2px_-2px_0_#000,2px_-2px_0_#000,-2px_2px_0_#000,_0_2px_0_#000,_2px_0_0_#000,_0_-2px_0_#000,_-2px_0_0_#000,2px_2px_5px_#000] uppercase text-white top-0'
+        className='font-meme absolute -translate-x-1/2 break-all w-4/5 text-center select-none left-1/2 my-4 mx-0 py-0 px-1 [text-shadow:2px_2px_0_#000,-2px_-2px_0_#000,2px_-2px_0_#000,-2px_2px_0_#000,_0_2px_0_#000,_2px_0_0_#000,_0_-2px_0_#000,_-2px_0_0_#000,2px_2px_5px_#000] uppercase text-white top-0'
         style={{ fontSize: `${fontSize}em` }}
       >
         {meme.text1}
