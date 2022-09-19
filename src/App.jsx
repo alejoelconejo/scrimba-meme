11e4b2938204532dffadd25f956bed4b36d75bc5
Alejo Janin <alejojanin@gmail.com>
2022-09-19 18:34:51 -0300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Meme from './components/Meme'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='font-sans text-white bg-gray-50 w-full h-full dark:bg-slate-900 transition-colors duration-500'>
       <Header />
       <Meme />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='mx-auto max-w-5xl text-black dark:text-white flex justify-end items-center py-2'>
+      Desarrollado por alejoelconejo 🐰
+    </footer>
+  )
+}
+
+export default Footer
