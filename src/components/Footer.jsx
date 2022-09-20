fbc9d246e92f9bc6cdd06be3e8e8edbe293a6b92
Alejo Janin <alejojanin@gmail.com>
2022-09-20 07:09:49 -0300

    Add x-padding to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='mx-auto max-w-5xl text-black dark:text-white flex justify-end items-center py-2'>
+    <footer className='mx-auto max-w-5xl px-3 text-black dark:text-white flex justify-end items-center py-2'>
       Desarrollado por alejoelconejo 🐰
     </footer>
   )
