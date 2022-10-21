d69aef2faf885648cb11b3bf4cde15cf4d85687f
Alejo Janin <alejojanin@gmail.com>
2022-10-21 07:33:55 -0300

    Modify App min-height

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Meme from './components/Meme'
 import Footer from './components/Footer'
 
 function App() {
-  return (
-    <div className='font-sans text-white bg-gray-100 w-full h-full dark:bg-slate-900 transition-colors duration-500'>
-      <Header />
-      <Meme />
-      <Footer />
-    </div>
-  )
+    return (
+        <div className='font-sans text-white bg-gray-100 w-full min-h-screen dark:bg-slate-900 transition-colors duration-500'>
+            <Header />
+            <Meme />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
