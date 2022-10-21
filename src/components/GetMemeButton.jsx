32fa93b1c6f3f9c0605a80ea023a1a5223b2974a
Alejo Janin <alejojanin@gmail.com>
2022-10-21 07:35:33 -0300

    Modify getMemeImage props

--- src/components/GetMemeButton.jsx
+++ src/components/GetMemeButton.jsx
@@ -2,17 +2,17 @@ import getMemeImage from '../services/getMemeImage'
 import { ReactComponent as GetMemeIcon } from '../images/get-meme-button.svg'
 
 const GetMemeButton = ({ allMemes, setMeme }) => {
-  return (
-    <button
-      className='rounded focus:ring-2 ring-violet-800 font-bold text-lg p-2 font-sans  bg-violet-500 w-fit mx-auto px-4 flex gap-2 items-end'
-      onClick={() => {
-        getMemeImage({ allMemes, setMeme })
-      }}
-    >
-      Get a new meme image
-      <GetMemeIcon className='h-6' />
-    </button>
-  )
+    return (
+        <button
+            className='rounded focus:ring-2 ring-violet-800 font-bold text-lg p-2 font-sans  bg-violet-500 w-fit mx-auto px-4 flex gap-2 items-end'
+            onClick={() => {
+                getMemeImage(allMemes, setMeme)
+            }}
+        >
+            Get a new meme image
+            <GetMemeIcon className='h-6' />
+        </button>
+    )
 }
 
 export default GetMemeButton
--- src/services/getMemeImage.js
+++ src/services/getMemeImage.js
@@ -1,7 +1,7 @@
-export default function getMemeImage({ allMemes, setMeme }) {
-  const randomNumber = Math.floor(Math.random() * allMemes.length)
-  const { url, name, id, box_count: countBox } = allMemes[randomNumber]
-  setMeme((prevMeme) => {
-    return { ...prevMeme, randomImage: url, name, id, countBox }
-  })
+export default function getMemeImage(allMemes, setMeme) {
+    const randomNumber = Math.floor(Math.random() * allMemes.length)
+    const { url, name, id, box_count: countBox } = allMemes[randomNumber]
+    setMeme((prevMeme) => {
+        return { ...prevMeme, randomImage: url, name, id, countBox }
+    })
 }
