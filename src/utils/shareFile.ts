b92001f17d696f0792fe5db21f54760eab72859c
Alejo Janin <alejojanin@gmail.com>
2022-12-08 16:43:53 -0300

    Modify share error message

--- src/utils/shareFile.ts
+++ src/utils/shareFile.ts
@@ -9,6 +9,8 @@ export const shareFile = (file: File, title: string, text: string) => {
       .then(() => console.log('Share was successful.'))
       .catch((error) => console.log('Sharing failed', error))
   } else {
-    console.log(`Your system doesn't support sharing files.`)
+    alert(
+      `😑 Your system doesn't support sharing files. Try with another browser!`
+    )
   }
 }
